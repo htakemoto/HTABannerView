41950264fa9177d0328271b38f7013a8afefc220
Hiroshi Takemoto <hiroshi.takemoto@outlook.com>
2014-04-25 12:11:30 -0400

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Takemoto on 4/25/14.
+//  Copyright (c) 2014 Takemoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by Takemoto on 4/25/14.
+//  Copyright (c) 2014 Takemoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
